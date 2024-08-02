affb638e837489d1b4a0e8809ee9b645df30ec93
Aditya Thakor <thakoraditya125@gmail.com>
2024-08-02 10:07:14 +0000

    add animations in main texts

--- src/App.jsx
+++ src/App.jsx
@@ -23,9 +23,27 @@ function App() {
                         </ul>
                     </nav>
                     <div id="header-text">
-                        <h1 className='welcome'>Welcome</h1>
-                        <h4 className='to'>to</h4>
-                        <h1 className="konoha">KONOHA</h1>
+                        <h1 className='welcome' aria-label="Welcome">
+                            <span aria-hidden="true">W</span>
+                            <span aria-hidden="true">e</span>
+                            <span aria-hidden="true">l</span>
+                            <span aria-hidden="true">c</span>
+                            <span aria-hidden="true">o</span>
+                            <span aria-hidden="true">m</span>
+                            <span aria-hidden="true">e</span>
+                        </h1>
+                        <h4 className='to' aria-label='to'>
+                            <span aria-hidden="true">t</span>
+                            <span aria-hidden="true">o</span>
+                        </h4>
+                        <h1 className="konoha" aria-label='Konoha'>
+                            <span aria-hidden="true">K</span>
+                            <span aria-hidden="true">O</span>
+                            <span aria-hidden="true">N</span>
+                            <span aria-hidden="true">O</span>
+                            <span aria-hidden="true">H</span>
+                            <span aria-hidden="true">A</span>
+                        </h1>
                     </div>
                     <div id="hedbtn">
                     <button className="get-started">Enroll Now!</button>
